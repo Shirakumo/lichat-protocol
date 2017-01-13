61c6c4800e056ecf7a5d14bf4a5034cd8c02c011
Shinmera <shinmera@tymoon.eu>
2017-01-13 10:40:13 +0100

    Catch ) at beginning of sexpr case

--- reader.lisp
+++ reader.lisp
@@ -94,6 +94,7 @@
                         (error 'incomplete-token))))
       (case char
         (#\( (read-sexpr-list stream))
+        (#\) (error 'incomplete-token))
         (#\" (read-sexpr-string stream))
         ((#\0 #\1 #\2 #\3 #\4 #\5 #\6 #\7 #\8 #\9 #\.)
          (unread-char char stream)
