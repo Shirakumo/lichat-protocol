325d0dbc8f8f4de0be56eb172ca93a4b66f506d7
Shinmera <shinmera@tymoon.eu>
2017-01-19 17:21:24 +0100

    Treat U+2007 as whitespace

--- reader.lisp
+++ reader.lisp
@@ -9,9 +9,10 @@
 (defvar *whitespace*
   #+asdf-unicode (map 'vector #'code-char '(#x0009 #x000A #x000B #x000C #x000D #x0020
                                             #x0085 #x00A0 #x1680 #x2000 #x2001 #x2002
-                                            #x2003 #x2004 #x2005 #x2006 #x2008 #x2009
-                                            #x200A #x2028 #x2029 #x202F #x205F #x3000
-                                            #x180E #x200B #x200C #x200D #x2060 #xFEFF))
+                                            #x2003 #x2004 #x2005 #x2006 #x2007 #x2008
+                                            #x2009 #x200A #x2028 #x2029 #x202F #x205F
+                                            #x3000 #x180E #x200B #x200C #x200D #x2060
+                                            #xFEFF))
   #-asdf-unicode (map 'vector #'code-char '(#x0009 #x000A #x000B #x000C #x000D #x0020)))
 (defvar *errors* NIL)
 (defvar *invalid-symbol* (make-symbol "INVALID-SYMBOL"))
